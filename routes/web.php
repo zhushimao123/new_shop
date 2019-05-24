b4fc06a73bb3d475f2e368646d6dee0ed20a2226
wanglei <1109073586@qq.com>
2019-05-24 01:15:54 +0000

    bug

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,12 @@
 */
 //首页
 Route::get('/', 'home\HomeController@index');
-<<<<<<< HEAD
+
 //商品详情
 Route::get('/detil', 'Goods\GoodsdetialController@detial');
 Route::post('/cart', 'Goods\GoodsdetialController@cart');
 Route::get('/cartdet', 'Cart\CartController@cartdet');
-=======
+
 //联系我们
 Route::get('/contact', 'contact\ContactController@contact');
 //接受数据
@@ -25,4 +25,4 @@ Route::post('sendmail', 'contact\ContactController@sendmail');
 //商品列表
 Route::get('/product', 'goodslist\GoodsListController@goodslist');
 
->>>>>>> fd25556f8c66cd56d07e899307a9d3a6ad3cbf72
+

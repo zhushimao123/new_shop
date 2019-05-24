c9824195e3429f3b3f981574da21f8e463a0d85d
wanglei <1109073586@qq.com>
2019-05-24 06:23:09 +0000

    购物车

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', 'home\HomeController@index');
 Route::get('/detil', 'Goods\GoodsdetialController@detial');
 Route::post('/cart', 'Goods\GoodsdetialController@cart');
 Route::get('/cartdet', 'Cart\CartController@cartdet');
+Route::post('/delete', 'Cart\CartController@delete');

296f2f2c5dbe5956200cd5126b6b25c9ac202d22
zhushimao123 <152359342622@163.com>
2019-05-27 07:11:49 +0000

    解决冲涂

--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -16,13 +16,8 @@ class CheckLogin
     public function handle($request, Closure $next)
     {
         //echo Session::get('user_name');die;
-<<<<<<< HEAD
-        if(Session::get('user_name')==null){
-            return redirect()->to("http://vm.them.com/login");
-=======
         if(Session::get('user_name')==null&&Session::get('user_id')==null){
             return redirect()->to("/login");
->>>>>>> 3bd5b49886da64e9c4370b7dbc986090da28fb06
         }
         return $next($request);
     }
